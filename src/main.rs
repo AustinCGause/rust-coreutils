e002217161335748a3dadbe90133e7916a24417a
AustinCGause <austincgause@gmail.com>
2024-03-15 01:21:20 -0400

    attempted to implement echo coreutil

--- src/main.rs
+++ src/main.rs
@@ -1,61 +1,31 @@
-use std::path::PathBuf;
+use clap::{
+    Parser,
+    Subcommand
+};
 
-use clap::{Parser, Subcommand};
+use echo::run;
+
+mod echo;
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
 struct Cli {
-    /// Optional name to operate on
-    name: Option<String>,
 
-    /// Sets a custom config file
-    #[arg(short, long, value_name = "FILE")]
-    config: Option<PathBuf>,
+    #[arg(short, long)]
+    echo: Vec<String>,
 
-    /// Turn debugging information on
-    #[arg(short, long, action = clap::ArgAction::Count)]
-    debug: u8,
+    #[arg(short, long)]
+    name: Vec<String>,
 
-    #[command(subcommand)]
-    command: Option<Commands>,
 }
 
 #[derive(Subcommand)]
 enum Commands {
-    /// does testing things
-    Test {
-        /// lists test values
-        #[arg(short, long)]
-        list: bool,
-    },
 }
 
 fn main() {
     let cli = Cli::parse();
 
-    if let Some(name) = cli.name.as_deref() {
-        println!("Value for name: {name}");
-    }
-
-    if let Some(config_path) = cli.config.as_deref() {
-        println!("Value for config: {}", config_path.display());
-    }
-
-    match cli.debug {
-        0 => println!("Debug mode is off"),
-        1 => println!("Debug mode is kind of on"),
-        2 => println!("Debug mode is on"),
-        _ => println!("Don't be crazy"),
-    }
-
-    match &cli.command {
-        Some(Commands::Test { list }) => {
-            if *list {
-                println!("Printing testing lists...");
-            } else {
-                println!("Not printing testing lists...");
-            }
-        }
-        None => {}
+    if !cli.echo.is_empty() {
     }
 }
